c82eac9a495023a430c80639db3a8d90cbd6fb16
Denis Davydov <davydden@gmail.com>
2017-10-11 15:33:05 +0200

    various cleanups and a remotely working script

--- dealii-spack-tests.sh
+++ dealii-spack-tests.sh
@@ -4,15 +4,18 @@
 #
 # Can be used with Cron job:
 # $crontab -e
-# * */2 * * * /path/to/dealii-auxiliary-scripts.sh
-#   ^^^ run every 2 hours
+# * * */1 * * /path/to/dealii-spack-tests.sh
+#     ^^^ run every day
 # p.s. check content by: crontab -l
+
+# Path to Spack:
+SPACK_ROOT=/home/davydden/spack
 
 # a commit in Spack to use:
 SPACK_COMMIT=57643ae84e95d3053d6bb8022b9de0420d151467
 
-# dealii specs (configuration) to test:
-#declare -a SPECS=('dealii%gcc@5.4.0+mpi^openmpi^openblas' 'dealii%gcc@5.4.0+mpi^openmpi^intel-mkl' 'dealii%gcc@5.4.0+mpi^openmpi^atlas' 'dealii%gcc@5.4.0+mpi+int64^openmpi^openblas' 'dealii%gcc@5.4.0+mpi^mpich^openblas');
+# dealii specs (configuration) to test in addition to settings in packages.yaml:
+#declare -a SPECS=('dealii+mpi^openmpi^openblas' 'dealii+mpi^openmpi^intel-mkl' 'dealii+mpi^openmpi^atlas' 'dealii+mpi+int64^openmpi^openblas' 'dealii+mpi^mpich^openblas');
 
 declare -a SPECS=('dealii+mpi^openmpi^openblas');
 
@@ -26,36 +29,55 @@ declare -a SPECS=('dealii+mpi^openmpi^openblas');
 # config:
 #  build_stage:
 #    - $spack/var/spack/stage
-# 3) environment-modules must be setup in .bashrc:
-# MODULES_HOME=$(spack location -i environment-modules)
-# source ${MODULES_HOME}/Modules/init/bash
-# . $SPACK_ROOT/share/spack/setup-env.sh
 
 
+#
+# =======================================================
+# DON'T EDIT BELOW
+# =======================================================
+#
+
+# First, setup path to Spack and environment-module
+export PATH=$SPACK_ROOT/bin:$PATH
+spack install environment-modules
+MODULES_HOME=$(spack location -i environment-modules)
+source ${MODULES_HOME}/Modules/init/bash
+. $SPACK_ROOT/share/spack/setup-env.sh
+
+# reset Spack to the desired commit:
 cd $SPACK_ROOT
 # git checkout develop
 # git pull
 # git reset --hard $SPACK_COMMIT
 
+# Install and load numdiff
 spack install numdiff
 spack load numdiff
+spack install cmake
+spack load cmake
 
+# Go through all the specs and test:
 for i in "${SPECS[@]}"
 do
-  cd $SPACK_ROOT
   # install dependencies
-  spack install --only dependencies "$i" || { echo 'failed to install $i' ; exit 1; }
+  spack install --only dependencies "$i" || { echo "failed to install $i" ; exit 1; }
   # install dealii and keep the stage folder
-  spack install --keep-stage "$i" || { echo 'failed to install $i' ; exit 1; }
+  spack install --keep-stage "$i" || { echo "failed to install $i" ; exit 1; }
   # go to the stage
   spack cd -s "$i"
-  # setup environement
-  spack env "$i" bash
+  cd dealii/spack-build
+  # setup environement to be exactly the same as during the buld of the spec
+  # spack env "$i" bash
   # setup / run / submit unit tests
   make -j8 setup_tests
   ctest -j8
   ctest -j8 -DDESCRIPTION="$i" -V -S ../tests/run_testsuite.cmake
   # exit spack env
-  exit
+  # exit
+  cd $SPACK_ROOT
+  # remove the current installation so that next time we build from scratch
+  # spack uninstall "$i"
+  # clean the stage:
+  # spack clean -s
 done
 
