6f04e0bf05ecca30936501d186d9573dde568b7e
Denis Davydov <davydden@gmail.com>
2017-10-13 19:03:44 +0200

    use spack env; extend to macOS; minor cleanup

--- dealii-spack-tests.sh
+++ dealii-spack-tests.sh
@@ -9,7 +9,7 @@
 # p.s. check content by: crontab -l
 
 # Path to Spack:
-SPACK_ROOT=/home/davydden/spack
+SPACK_ROOT=$HOME/spack
 
 # a commit in Spack to use:
 SPACK_COMMIT=e8073970743e80e375d804b626ec64eaacd4da20
@@ -23,14 +23,31 @@ declare -a SPECS=(
 '^openmpi^atlas'
 '+int64^openmpi^openblas'
 '^mpich^openblas'
-'+optflags^openmpi^openblas');
+'+optflags^openmpi^openblas'
+);
 
 # =======================================================
 # DON'T EDIT BELOW
 # =======================================================
 
+# echo status
+secho() {
+  echo -e "\033[0;94m==> \033[0m$@"
+}
+
+# error
+becho() {
+  echo -e "\033[4;31mError:\033[0m $@"
+}
+
 # Get number of cores:
-NP=$(nproc --all)
+if [ -x /usr/bin/sw_vers ]; then
+  NP=$(sysctl -n hw.ncpu)
+else
+  NP=$(nproc --all)
+fi
+
+secho "Will use $NP processes"
 
 # First, setup path to Spack and environment-module
 export PATH=$SPACK_ROOT/bin:$PATH
@@ -48,34 +65,32 @@ git reset --hard $SPACK_COMMIT
 # Install and load numdiff
 spack install numdiff
 spack load numdiff
-# FIXME: remove when spack env can be called from bash
-spack install cmake
-spack load cmake
 
 # Go through all the specs and test:
 for i in "${SPECS[@]}"
 do
   # current spec:
   s="$BASE_SPEC$i"
+  secho "Testing: $s"
+  # clean the stage:
+  spack clean -s
   # install dependencies
-  spack install --only dependencies "$s" || { echo "failed to install $s" ; exit 1; }
+  spack install --only dependencies "$s" || { becho "Failed to install $s" ; exit 1; }
   # install dealii and keep the stage folder
-  spack install --keep-stage "$s" || { echo "failed to install $s" ; exit 1; }
+  spack install --keep-stage "$s" || { becho "Failed to install $s" ; exit 1; }
   # go to the stage
   spack cd -s "$s"
   cd dealii/spack-build
-  # setup environement to be exactly the same as during the buld of the spec
-  # spack env "$s" bash
-  # setup / run / submit unit tests
-  make -j"$NP" setup_tests
-  ctest -j"$NP"
-  ctest -j"$NP" -DDESCRIPTION="$i" -V -S ../tests/run_testsuite.cmake
-  # exit spack env
-  # exit
+  # setup environement to be exactly the same as during the buld of the spec.
+  # then setup / run / submit unit tests via here document
+  spack env "$s" bash <<EOF
+make -j"$NP" setup_tests
+ctest -j"$NP"
+ctest -j"$NP" -DDESCRIPTION="$i" -V -S ../tests/run_testsuite.cmake
+EOF
   cd $SPACK_ROOT
   # remove the current installation so that next time we build from scratch
-  spack uninstall -y "$s"
+  spack uninstall -a -y "$s"
   # clean the stage:
   spack clean -s
 done
-
